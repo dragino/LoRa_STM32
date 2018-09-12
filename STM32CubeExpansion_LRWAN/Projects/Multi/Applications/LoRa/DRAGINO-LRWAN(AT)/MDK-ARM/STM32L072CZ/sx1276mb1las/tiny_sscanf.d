004acdb7d18de6c7c2601de89e58be2c9bf2e90d
goodcheney <goodcheney@naver.com>
2018-09-12 17:36:13 +0800

    111

--- STM32CubeExpansion_LRWAN/Projects/Multi/Applications/LoRa/DRAGINO-LRWAN(AT)/MDK-ARM/STM32L072CZ/sx1276mb1las/tiny_sscanf.d
+++
@@ -1,8 +0,0 @@
-.\sx1276mb1las\tiny_sscanf.o: ..\..\src\tiny_sscanf.c
-.\sx1276mb1las\tiny_sscanf.o: D:\ARM\ARM\ARMCC\Bin\..\include\stdio.h
-.\sx1276mb1las\tiny_sscanf.o: D:\ARM\ARM\ARMCC\Bin\..\include\string.h
-.\sx1276mb1las\tiny_sscanf.o: D:\ARM\ARM\ARMCC\Bin\..\include\stdarg.h
-.\sx1276mb1las\tiny_sscanf.o: D:\ARM\ARM\ARMCC\Bin\..\include\stdint.h
-.\sx1276mb1las\tiny_sscanf.o: D:\ARM\ARM\ARMCC\Bin\..\include\stdlib.h
-.\sx1276mb1las\tiny_sscanf.o: D:\ARM\ARM\ARMCC\Bin\..\include\inttypes.h
-.\sx1276mb1las\tiny_sscanf.o: ..\..\inc\tiny_sscanf.h
--- STM32CubeExpansion_LRWAN/Projects/Multi/Applications/LoRa/DRAGINO-LRWAN(AT)/MDK-ARM/STM32L072CZ/sx1276mb1las/tiny_vsnprintf.d
+++
@@ -1,4 +0,0 @@
-.\sx1276mb1las\tiny_vsnprintf.o: ..\..\src\tiny_vsnprintf.c
-.\sx1276mb1las\tiny_vsnprintf.o: D:\ARM\ARM\ARMCC\Bin\..\include\stdarg.h
-.\sx1276mb1las\tiny_vsnprintf.o: D:\ARM\ARM\ARMCC\Bin\..\include\string.h
-.\sx1276mb1las\tiny_vsnprintf.o: ..\..\inc\tiny_vsnprintf.h
